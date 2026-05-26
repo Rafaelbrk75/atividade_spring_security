3758b8596fb686ad927d9888fbce329ca81dd04f
Rafaelbrk75 <rafaelbrk75@gmail.com>
2026-05-26 19:01:23 -0300

    iniciando atividade

---
+++ src/main/java/br/edu/unicesumar/atividade_spring_security/AtividadeSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.atividade_spring_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtividadeSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/unicesumar/atividade_spring_security/AtividadeSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.unicesumar.atividade_spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
